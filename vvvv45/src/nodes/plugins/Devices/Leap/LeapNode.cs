531bb5c9ac11c254daf83b3b3f12c2e7817b184e
phleg ma <buhlert@meso.net>
2013-04-04 10:32:50 +0200

    Leap stuff

--- vvvv45/src/nodes/plugins/Devices/Leap/LeapNode.cs
+++ vvvv45/src/nodes/plugins/Devices/Leap/LeapNode.cs
@@ -17,57 +17,79 @@ namespace VVVV.Nodes.Devices.Leap
 {
 	#region PluginInfo
 	[PluginInfo(Name = "Leap",
-	Category = "Devices", 
-	Help = "Returns the tracking data of the Leap device",
-	Tags = "tracking, hand, finger",
-	AutoEvaluate = true)]
+	            Category = "Devices",
+	            Help = "Returns the tracking data of the Leap device",
+	            Tags = "tracking, hand, finger",
+	            AutoEvaluate = true)]
 	#endregion PluginInfo
 	public class LeapNode : IPluginEvaluate, IDisposable
 	{
 		#region fields & pins
 		#pragma warning disable 0649
 
-        [Output("Hand Position")]
-        ISpread<Vector3D> FHandPosOut;
-        
-        [Output("Hand Direction")]
-        ISpread<Vector3D> FHandDirOut;
-        
-        [Output("Hand Normal")]
-        ISpread<Vector3D> FHandNormOut;
-        
-        [Output("Hand Ball Center")]
-        ISpread<Vector3D> FHandBallCentOut;
-        
-        [Output("Hand Ball Radius")]
-        ISpread<double> FHandBallRadOut;
-        
-        [Output("Hand Velocity")]
-        ISpread<Vector3D> FHandVelOut;
-        
-        [Output("Hand ID")]
-        ISpread<int> FHandIDOut;
-        
-        [Output("Is Tool")]
-        ISpread<bool> FFingerIsToolOut;
-        
-        [Output("Finger Position")]
-        ISpread<Vector3D> FFingerPosOut;
-        
-        [Output("Finger Direction")]
-        ISpread<Vector3D> FFingerDirOut;
-        
-        [Output("Finger Velocity")]
-        ISpread<Vector3D> FFingerVelOut;
-        
-        [Output("Finger Size")]
-        ISpread<Vector2D> FFingerSizeOut;
-        
-        [Output("Finger ID")]
-        ISpread<int> FFingerIDOut;
-        
-        [Output("Hand Slice")]
-        ISpread<int> FHandSliceOut;
+		[Output("Hand Position")]
+		ISpread<Vector3D> FHandPosOut;
+		
+		[Output("Hand Direction")]
+		ISpread<Vector3D> FHandDirOut;
+		
+		[Output("Hand Normal")]
+		ISpread<Vector3D> FHandNormOut;
+		
+		[Output("Hand Ball Center")]
+		ISpread<Vector3D> FHandBallCentOut;
+		
+		[Output("Hand Ball Radius")]
+		ISpread<double> FHandBallRadOut;
+		
+		[Output("Hand Velocity")]
+		ISpread<Vector3D> FHandVelOut;
+		
+		[Output("Hand ID")]
+		ISpread<int> FHandIDOut;
+		
+		[Output("Is Tool")]
+		ISpread<bool> FFingerIsToolOut;
+		
+		[Output("Finger Position")]
+		ISpread<Vector3D> FFingerPosOut;
+		
+		[Output("Finger Direction")]
+		ISpread<Vector3D> FFingerDirOut;
+		
+		[Output("Finger Velocity")]
+		ISpread<Vector3D> FFingerVelOut;
+		
+		[Output("Finger Size")]
+		ISpread<Vector2D> FFingerSizeOut;
+		
+		[Output("Finger ID")]
+		ISpread<int> FFingerIDOut;
+		
+		[Output("Hand Slice")]
+		ISpread<int> FHandSliceOut;
+		
+		//Screens
+		[Input("Update Screens", IsBang=true)]
+		IDiffSpread<bool> FUpdateScreensIn;
+		
+		[Input("CropRatio", IsSingle=true ,DefaultValue=1.0)]
+		IDiffSpread<float> FCropRatio;
+		
+		[Output("Screens")]
+		ISpread<Screen> FScreensOut;
+		
+		[Output("X")]
+		ISpread<int> FXOut;
+		
+		[Output("Y")]
+		ISpread<int> FYOut;
+		
+		[Output("ProjectionPoint")]
+		ISpread<Vector3D> FProjectionPointOut;
+		
+		[Output("PointingDistance")]
+		ISpread<float> FPointingDistance;
 		#pragma warning restore
 		
 		Controller FLeapController = new Controller();
@@ -77,10 +99,10 @@ namespace VVVV.Nodes.Devices.Leap
 		//called when data for any output pin is requested
 		public void Evaluate(int SpreadMax)
 		{
+			
 			if(FLeapController.IsConnected)
 			{
 				var hands = FLeapController.Frame().Hands;
-				
 				SpreadMax = hands.Count;
 				
 				FHandPosOut.SliceCount = SpreadMax;
@@ -98,6 +120,8 @@ namespace VVVV.Nodes.Devices.Leap
 				FFingerSizeOut.SliceCount = 0;
 				FFingerIDOut.SliceCount = 0;
 				FHandSliceOut.SliceCount = 0;
+				FXOut.SliceCount = 0;
+				FYOut.SliceCount = 0;
 				
 				for(int i=0; i < SpreadMax; i++)
 				{
@@ -126,8 +150,37 @@ namespace VVVV.Nodes.Devices.Leap
 						FFingerSizeOut.Add(new Vector2D(pointable.Width * 0.001, pointable.Length * 0.001));
 						FFingerIDOut.Add(pointable.Id);
 						FHandSliceOut.Add(i);
+						
+						
+						ScreenList screens = FLeapController.CalibratedScreens;
+						Screen screen = screens.ClosestScreenHit(pointable);
+
+						Vector normalizedCoordinates = screen.Intersect(pointable, true, 1);
+						FXOut.Add((int)(normalizedCoordinates.x * screen.WidthPixels));
+						FYOut.Add(screen.HeightPixels - (int)(normalizedCoordinates.y * screen.HeightPixels));
+						
+						FProjectionPointOut[i] = screen.Project(pointable.TipPosition, false).ToVector3DPos();
+						
+						Vector intersection = screen.Intersect(pointable, false);
+						Vector tipToScreen = intersection - pointable.TipPosition;
+						FPointingDistance[i] = tipToScreen.Magnitude * (float) 0.001;
+					}
+					
+					
+					
+					if(FUpdateScreensIn.IsChanged)
+					{
+						int ScreenCount = FLeapController.CalibratedScreens.Count;
+						ScreenList Screens = FLeapController.CalibratedScreens;
+						FScreensOut.SliceCount = ScreenCount;
+						
+						for (int k = 0; k < ScreenCount; k++)
+						{
+							FScreensOut[k] = Screens[k];
+						}
 					}
 				}
+				
 			}
 			
 		}
---
+++ vvvv45/src/nodes/plugins/Devices/Leap/ScreenNode.cs
@@ -0,0 +1,86 @@
+﻿#region usings
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.Algorithm;
+using VVVV.Utils.VColor;
+using VVVV.Utils.VMath;
+
+using Leap;
+
+#endregion usings
+
+namespace VVVV.Nodes.Devices.Leap
+{
+	#region PluginInfo
+	[PluginInfo(Name = "Screen",
+	Category = "Devices",
+	Version = "Leap",
+	Help = "Creates a virtuel screen for the leap device",
+	Tags = "tracking, hand, finger",
+	AutoEvaluate = false)]
+	#endregion PluginInfo
+	public class ScreenNode : IPluginEvaluate
+	{
+		#region fields & pins
+		#pragma warning disable 0649
+
+        
+        [Input("Screens")]
+        IDiffSpread<Screen> FScreensIn;
+        
+        [Output("ID")]
+        ISpread<int> FIdOut;
+        
+        [Output("BottomLeftCorner")]
+        ISpread<Vector3D> FBLCornerOut;
+        
+        [Output("Height")]
+        ISpread<int> FHeightOut;
+        
+        [Output("Width")]
+        ISpread<int> FWidthOut;
+        
+        [Output("HorizontalAxis")]
+        ISpread<Vector3D> FHAxisOut;
+        
+        [Output("VerticalAxis")]
+        ISpread<Vector3D> FVAxisOut;
+		#pragma warning restore
+	
+		
+		#endregion fields & pins
+		
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax)
+		{
+			if(FScreensIn.IsChanged)
+			{
+				FIdOut.SliceCount = SpreadMax;
+				FBLCornerOut.SliceCount = SpreadMax;
+				FHeightOut.SliceCount = SpreadMax;
+				FWidthOut.SliceCount = SpreadMax;
+				FHAxisOut.SliceCount = SpreadMax;
+				FVAxisOut.SliceCount = SpreadMax;
+				
+				for (int i = 0; i < SpreadMax; i++) 
+				{
+					FIdOut[i] = FScreensIn[i].Id;
+					FBLCornerOut[i] = FScreensIn[i].BottomLeftCorner.ToVector3DPos();
+					FHeightOut[i] = FScreensIn[i].HeightPixels;
+					FWidthOut[i] = FScreensIn[i].WidthPixels;
+					FHAxisOut[i] = FScreensIn[i].HorizontalAxis.ToVector3DPos();
+					FVAxisOut[i] = FScreensIn[i].VerticalAxis.ToVector3DPos();
+ 				}
+			}
+			
+		}
+	}
+}
+
+
+
+
