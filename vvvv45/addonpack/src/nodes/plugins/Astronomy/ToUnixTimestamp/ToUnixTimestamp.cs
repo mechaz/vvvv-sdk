3cf34b47fc20162ac58b123e6d71d629eeb98215
Nikos Mechanezidis <mechanezidis@meso.net>
2013-05-07 19:29:40 +0200

    added TimeZoneInfo selection, still buggy

--- vvvv45/addonpack/src/nodes/plugins/Astronomy/ToUnixTimestamp/ToUnixTimestamp.cs
+++ vvvv45/addonpack/src/nodes/plugins/Astronomy/ToUnixTimestamp/ToUnixTimestamp.cs
@@ -2,38 +2,72 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using VVVV.Utils.VColor;
 using VVVV.Utils.VMath;
 
+
 using VVVV.PluginInterfaces.V1;
 using VVVV.PluginInterfaces.V2;
+using VVVV.Core.Logging;
+
 
 #endregion usings
 
 namespace VVVV.Nodes
 {
-	public class ToUnixTimestamp : IPlugin, IDisposable
+
+    #region PluginInfo
+    [PluginInfo(Name = "ToUnixTimestamp", Category = "Astronomy", Version = "1.0", Help = "ToUnixTimestamp Node", Tags = "timestamp, datetime, unix", Author = "niggos")]
+    #endregion PluginInfo
+
+	public class ToUnixTimestamp : IPluginEvaluate
 	{
+
+
 		
         #region field declaration
-        //the host (mandatory)
-        private IPluginHost FHost;
-        // Track whether Dispose has been called.
-        private bool FDisposed = false;
 
+        const string DATETIME_ENUM_NAME = "DatetimeFormatENUM";
+        const string FORMAT_A = "yyyy.MM.dd-hh.mm.ss";
+        const string FORMAT_B = "dd-MM-yyyy hh:mm:ss";
+        const string FORMAT_C = "yyyy-MM-dd";
+        const string FORMAT_D = "dd-MM-yyyy";
+        string[] dtFormats = { FORMAT_A, FORMAT_B, FORMAT_C, FORMAT_D };
+
+        private TimeZoneInfo TIMEZONEINFO_UTC;
+
+        const string TIMEZONE_ENUM_NAME = "TimezoneENUM";
         // input pin declaration
-        private IStringIn FInCurrentTime;
-        private IValueIn FInHourOffset;
-        private IEnumIn FInDateTimeFormat;
-        string[] enumEntries = { "uno", "due", "tres" };
-        const string DATETIME_ENUM_NAME = "EiDesEnumJunge";
+        [Input("current time", DefaultString = "1970-01-01 00:00:00")]
+        ISpread<string> FInCurrentTime;
 
+        [Input("datetime format", EnumName=DATETIME_ENUM_NAME, IsSingle=true)]
+        ISpread<EnumEntry> FInDateTimeFormat;
 
         // output pins
-        private IValueOut FOutConvertedTime;
-        private IValueOut FOutCurrentTime;
+        [Output("current time")]
+        ISpread<int> FOutCurrentTime;
 
-        
+        [Output("converted time")]
+        ISpread<int> FOutConvertedTime;
+
+
+        [Input("time zone", EnumName = TIMEZONE_ENUM_NAME, IsSingle = true)]
+        IDiffSpread<EnumEntry> FInTimeZone;
+
+
+
+
+
+        private bool init = true;
+
+        string[] tziNames;
+        List<TimeZoneInfo> lsTZInfo;
+
+        private TimeZoneInfo FCurrentTZI;
+        private int FTsOffsetSeconds;
+
 
         #endregion
 
@@ -42,154 +76,13 @@ namespace VVVV.Nodes
 
         public ToUnixTimestamp()
         {
-            //the nodes constructor
-            //nothing to declare for this node
-            EnumManager.UpdateEnum(DATETIME_ENUM_NAME, enumEntries[0], enumEntries);
+
         }
 
-        // Implementing IDisposable's Dispose method.
-        // Do not make this method virtual.
-        // A derived class should not be able to override this method.
-        public void Dispose()
-        {
-            Dispose(true);
-            // Take yourself off the Finalization queue
-            // to prevent finalization code for this object
-            // from executing a second time.
-            GC.SuppressFinalize(this);
-        }
-
-        // Dispose(bool disposing) executes in two distinct scenarios.
-        // If disposing equals true, the method has been called directly
-        // or indirectly by a user's code. Managed and unmanaged resources
-        // can be disposed.
-        // If disposing equals false, the method has been called by the
-        // runtime from inside the finalizer and you should not reference
-        // other objects. Only unmanaged resources can be disposed.
-        protected virtual void Dispose(bool disposing)
-        {
-            // Check to see if Dispose has already been called.
-            if (!FDisposed)
-            {
-                // Release unmanaged resources. If disposing is false,
-                // only the following code is executed.
-
-
-                // Note that this is not thread safe.
-                // Another thread could start disposing the object
-                // after the managed resources are disposed,
-                // but before the disposed flag is set to true.
-                // If thread safety is necessary, it must be
-                // implemented by the client.
-            }
-            FDisposed = true;
-        }
-
-        // Use C# destructor syntax for finalization code.
-        // This destructor will run only if the Dispose method
-        // does not get called.
-        // It gives your base class the opportunity to finalize.
-        // Do not provide destructors in types derived from this class.
-        ~ToUnixTimestamp()
-        {
-            // Do not re-create Dispose clean-up code here.
-            // Calling Dispose(false) is optimal in terms of
-            // readability and maintainability.
-            Dispose(false);
-        }
         #endregion constructor/destructor
 
 
-        #region pin creation
-
-        //this method is called by vvvv when the node is created
-        public void SetPluginHost(IPluginHost Host)
-        {
-            //assign host
-            FHost = Host;
-
-            //create inputs
-            FHost.CreateStringInput("Current Time (yyyy-MM-dd hh:mm:ss)", TSliceMode.Dynamic, TPinVisibility.True, out FInCurrentTime);
-            FInCurrentTime.SetSubType("", false);
-
-            FHost.CreateValueInput("hour offset (to UTC)", 1, null, TSliceMode.Dynamic, TPinVisibility.True, out FInHourOffset);
-            FInHourOffset.SetSubType(-12, 12, 1, 0, false, false, true);
-
-            FHost.CreateEnumInput(DATETIME_ENUM_NAME, TSliceMode.Dynamic, TPinVisibility.True, out FInDateTimeFormat);
-            EnumManager.UpdateEnum(DATETIME_ENUM_NAME, enumEntries[0], enumEntries);
-            
-            //create outputs	    	
-            FHost.CreateValueOutput("Converted Time", 1, null, TSliceMode.Dynamic, TPinVisibility.True, out FOutConvertedTime);
-            FHost.CreateValueOutput("Current Time", 1, null, TSliceMode.Dynamic, TPinVisibility.True, out FOutCurrentTime);
-        }
-
-        #endregion pin creation
-
-
-        #region node name and infos
-
-        //provide node infos 
-        private static IPluginInfo FPluginInfo;
-        public static IPluginInfo PluginInfo
-        {
-            get
-            {
-                if (FPluginInfo == null)
-                {
-                    //fill out nodes info
-                    //see: http://www.vvvv.org/tiki-index.php?page=Conventions.NodeAndPinNaming
-                    FPluginInfo = new PluginInfo();
-
-                    //the nodes main name: use CamelCaps and no spaces
-                    FPluginInfo.Name = "ToUnixTimestamp";
-                    //the nodes category: try to use an existing one
-                    FPluginInfo.Category = "Astronomy";
-                    //the nodes version: optional. leave blank if not
-                    //needed to distinguish two nodes of the same name and category
-                    FPluginInfo.Version = "";
-
-                    //the nodes author: your sign
-                    FPluginInfo.Author = "niggos";
-                    //describe the nodes function
-                    FPluginInfo.Help = "Conerts a given time to unix timestamp";
-                    //specify a comma separated list of tags that describe the node
-                    FPluginInfo.Tags = "";
-
-                    //give credits to thirdparty code used
-                    FPluginInfo.Credits = "phlegma";
-                    //any known problems?
-                    FPluginInfo.Bugs = "";
-                    //any known usage of the node that may cause troubles?
-                    FPluginInfo.Warnings = "";
-
-                    //leave below as is
-                    System.Diagnostics.StackTrace st = new System.Diagnostics.StackTrace(true);
-                    System.Diagnostics.StackFrame sf = st.GetFrame(0);
-                    System.Reflection.MethodBase method = sf.GetMethod();
-                    FPluginInfo.Namespace = method.DeclaringType.Namespace;
-                    FPluginInfo.Class = method.DeclaringType.Name;
-                    //leave above as is
-                }
-                return FPluginInfo;
-            }
-        }
-
-        public bool AutoEvaluate
-        {
-            //return true if this node needs to calculate every frame even if nobody asks for its output
-            get { return true; }
-        }
-
-        #endregion node name and infos
-
-
         #region mainloop
-
-        public void Configurate(IPluginConfig Input)
-        {
-            //nothing to configure in this plugin
-            //only used in conjunction with inputs of type cmpdConfigurate
-        }
 
 
         // method called by vvvv each frame
@@ -197,27 +90,84 @@ namespace VVVV.Nodes
         {
             FOutConvertedTime.SliceCount = SpreadMax;
             FOutCurrentTime.SliceCount = SpreadMax;
-            
+
             for (int i = 0; i < SpreadMax; i++)
             {
+                if (!init)
+                {
+                    try
+                    {
+                        if (FInTimeZone.IsChanged)
+                        {
+                            FCurrentTZI = GetTimeZoneInfoByName(FInTimeZone[0].ToString());
+                            FTsOffsetSeconds = Convert.ToInt32(FCurrentTZI.BaseUtcOffset.TotalSeconds);
+                        }
+                        string format = FInDateTimeFormat[0].Name;
+                        FOutConvertedTime[i] = conv_DateTimeToTotalSeconds(conv_StringToDateTime_2(FInCurrentTime[i], format));
+                        FOutCurrentTime[i] = conv_UTCToTotalSeconds() + Convert.ToInt32(FTsOffsetSeconds);
+                        string s = "";
+                    }
+                    catch (Exception e)
+                    {
+                        // add FLogger here, currently problem using The Import command
+                        string t = e.Message;
+                        int s = 0;
+                    }
+                }
+            }
+
+            if (init) 
+            {
+                EnumManager.UpdateEnum(DATETIME_ENUM_NAME, dtFormats[0], dtFormats);
                 try
                 {
-                    double offset;
-                    string timeIn;
-                    FInCurrentTime.GetString(i, out timeIn);
-                    FInHourOffset.GetValue(i, out offset);
-
-                    FOutConvertedTime.SetValue(i, conv_DateTimeToTotalSeconds( conv_StringToDateTime(timeIn) ) );
-                    FOutCurrentTime.SetValue(i, conv_UTCToTotalSeconds() + Convert.ToInt64(offset * 60 * 60));
+                    lsTZInfo = GetAllTimezones();
+                    tziNames = new string[lsTZInfo.Count];
+                    for (int j = 0; j < lsTZInfo.Count; j++)
+                    {
+                        tziNames[j] = lsTZInfo[j].DisplayName;
+                    }
+                    EnumManager.UpdateEnum(TIMEZONE_ENUM_NAME, "(UTC) Coordinated Universal Time", tziNames);
+                    init = false;
                 }
                 catch (Exception e)
                 {
-                    
+                    // add FLogger here, currently problem using The Import command
                 }
+
             }
         }
 
         #endregion mainloop
+
+
+        private TimeZoneInfo GetTimeZoneInfoByName(string name)
+        {
+            TimeZoneInfo tz = System.TimeZoneInfo.Local;
+            for (int i = 0; i < tziNames.Length; i++) 
+            {
+                if (name.Equals(tziNames[i]))
+                {
+                    tz = lsTZInfo[i];
+                    break;
+                }
+            }
+            return tz;
+        }
+
+
+        private List<TimeZoneInfo> GetAllTimezones()
+        {
+            List<TimeZoneInfo> ls = new List<TimeZoneInfo>();
+            ReadOnlyCollection<TimeZoneInfo> roc = TimeZoneInfo.GetSystemTimeZones();
+            for (int i = 0; i < roc.Count; i++)
+            {
+                string name = roc[i].DisplayName;
+                ls.Add(roc[i]);
+            }
+            return ls;
+        }
+
 
         private int conv_DateTimeToTotalSeconds(DateTime dt)
         {
@@ -250,6 +200,57 @@ namespace VVVV.Nodes
             return dt;
         }
 
+        private DateTime conv_StringToDateTime_2(string t, string format)
+        {
+            int y, M, d, h, m, s, year, month, da, hour, minute, second;
+            DateTime dt;
+            y = format.IndexOf("yyyy");
+            M = format.IndexOf("MM");
+            d = format.IndexOf("dd");
+            h = format.IndexOf("hh");
+            m = format.IndexOf("mm");
+            s = format.IndexOf("ss");
+
+            if (format.Equals(FORMAT_A))
+            {
+                y = 0; M = 5; d = 8; h = 11; m = 14; s = 17;
+                dt = new DateTime(Convert.ToInt32(t.Substring(y, 4)), 
+                                  Convert.ToInt32(t.Substring(M, 2)), 
+                                  Convert.ToInt32(t.Substring(d, 2)), 
+                                  Convert.ToInt32(t.Substring(h, 2)), 
+                                  Convert.ToInt32(t.Substring(m, 2)), 
+                                  Convert.ToInt32(t.Substring(s, 2)));
+            }
+            else if (format.Equals(FORMAT_B)) 
+            {
+                y = 6; M = 3; d = 0; h = 11; m = 14; s = 17;
+                dt = new DateTime(Convert.ToInt32(t.Substring(y, 4)),
+                                  Convert.ToInt32(t.Substring(M, 2)),
+                                  Convert.ToInt32(t.Substring(d, 2)),
+                                  Convert.ToInt32(t.Substring(h, 2)),
+                                  Convert.ToInt32(t.Substring(m, 2)),
+                                  Convert.ToInt32(t.Substring(s, 2)));
+            }
+            else if (format.Equals(FORMAT_C))
+            {
+                y = 0; M = 5; d = 8;
+                dt = new DateTime(Convert.ToInt32(t.Substring(y, 4)),
+                                  Convert.ToInt32(t.Substring(M, 2)),
+                                  Convert.ToInt32(t.Substring(d, 2)));
+            }
+            else if (format.Equals(FORMAT_D))
+            {
+                y = 6; M = 3; d = 0;
+                dt = new DateTime(Convert.ToInt32(t.Substring(y, 4)),
+                                  Convert.ToInt32(t.Substring(M, 2)),
+                                  Convert.ToInt32(t.Substring(d, 2)));
+            }
+            else
+            {
+                dt = new DateTime(1970, 01, 01, 01, 01, 01);
+            }
+            return dt;
+        }
 
         private int conv_UTCToTotalSeconds()
         {
