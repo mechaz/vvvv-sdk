4dfeca0ac3e1c0ec9b3e5be8c836a626cb26771f
Nikos Mechanezidis <mechanezidis@meso.net>
2013-05-08 18:41:12 +0200

    removed enum list of worldzones, added enable utc offset

--- vvvv45/addonpack/src/nodes/plugins/Astronomy/ToUnixTimestamp/ToUnixTimestamp.cs
+++ vvvv45/addonpack/src/nodes/plugins/Astronomy/ToUnixTimestamp/ToUnixTimestamp.cs
@@ -23,54 +23,32 @@ namespace VVVV.Nodes
 
 	public class ToUnixTimestamp : IPluginEvaluate
 	{
-
-
-		
         #region field declaration
 
-        const string DATETIME_ENUM_NAME = "DatetimeFormatENUM";
-        const string FORMAT_A = "yyyy.MM.dd-hh.mm.ss";
-        const string FORMAT_B = "dd-MM-yyyy hh:mm:ss";
-        const string FORMAT_C = "yyyy-MM-dd";
-        const string FORMAT_D = "dd-MM-yyyy";
-        string[] dtFormats = { FORMAT_A, FORMAT_B, FORMAT_C, FORMAT_D };
+        // used datetimeformat for conversion: "yyyy.MM.dd-hh.mm.ss";
 
-        private TimeZoneInfo TIMEZONEINFO_UTC;
-
-        const string TIMEZONE_ENUM_NAME = "TimezoneENUM";
         // input pin declaration
-        [Input("current time", DefaultString = "1970-01-01 00:00:00")]
+        [Input("Conversion Time", DefaultString = "1970-01-01 00:00:00")]
         ISpread<string> FInCurrentTime;
 
-        [Input("datetime format", EnumName=DATETIME_ENUM_NAME, IsSingle=true)]
-        ISpread<EnumEntry> FInDateTimeFormat;
+        [Input("Enable UTC Offset", IsToggle = true, IsSingle = true, DefaultBoolean = false)]
+        ISpread<bool> FInEnableUTCOffset;
 
         // output pins
-        [Output("current time")]
+        [Output("Current Time")]
         ISpread<int> FOutCurrentTime;
 
-        [Output("converted time")]
+        [Output("Converted Time")]
         ISpread<int> FOutConvertedTime;
 
 
-        [Input("time zone", EnumName = TIMEZONE_ENUM_NAME, IsSingle = true)]
-        IDiffSpread<EnumEntry> FInTimeZone;
+        [Input("UTC Offset", DefaultValue=0, MinValue=-12, MaxValue=12, IsSingle=true)]
+        IDiffSpread<int> FInUTCOffset;
 
-
-
-
-
-        private bool init = true;
-
-        string[] tziNames;
-        List<TimeZoneInfo> lsTZInfo;
-
-        private TimeZoneInfo FCurrentTZI;
-        private int FTsOffsetSeconds;
-
+        private int FUTCOffset;
+        private bool FUseUTCOffset = false;
 
         #endregion
-
 
         #region constructor/destructor
 
@@ -90,84 +68,35 @@ namespace VVVV.Nodes
         {
             FOutConvertedTime.SliceCount = SpreadMax;
             FOutCurrentTime.SliceCount = SpreadMax;
+            
+            if (FInUTCOffset.IsChanged)
+            {
+                FUTCOffset = FInUTCOffset[0];
+            }
 
             for (int i = 0; i < SpreadMax; i++)
             {
-                if (!init)
-                {
-                    try
-                    {
-                        if (FInTimeZone.IsChanged)
-                        {
-                            FCurrentTZI = GetTimeZoneInfoByName(FInTimeZone[0].ToString());
-                            FTsOffsetSeconds = Convert.ToInt32(FCurrentTZI.BaseUtcOffset.TotalSeconds);
-                        }
-                        string format = FInDateTimeFormat[0].Name;
-                        FOutConvertedTime[i] = conv_DateTimeToTotalSeconds(conv_StringToDateTime_2(FInCurrentTime[i], format));
-                        FOutCurrentTime[i] = conv_UTCToTotalSeconds() + Convert.ToInt32(FTsOffsetSeconds);
-                        string s = "";
-                    }
-                    catch (Exception e)
-                    {
-                        // add FLogger here, currently problem using The Import command
-                        string t = e.Message;
-                        int s = 0;
-                    }
-                }
-            }
-
-            if (init) 
-            {
-                EnumManager.UpdateEnum(DATETIME_ENUM_NAME, dtFormats[0], dtFormats);
                 try
                 {
-                    lsTZInfo = GetAllTimezones();
-                    tziNames = new string[lsTZInfo.Count];
-                    for (int j = 0; j < lsTZInfo.Count; j++)
+                    if (FInEnableUTCOffset[i])
                     {
-                        tziNames[j] = lsTZInfo[j].DisplayName;
+                        FOutConvertedTime[i] = conv_DateTimeToTotalSeconds(conv_StringToDateTime_2(FInCurrentTime[i])) + Convert.ToInt32(FUTCOffset * 3600);
+                        FOutCurrentTime[i] = conv_UTCToTotalSeconds() + Convert.ToInt32(FUTCOffset * 3600);
                     }
-                    EnumManager.UpdateEnum(TIMEZONE_ENUM_NAME, "(UTC) Coordinated Universal Time", tziNames);
-                    init = false;
+                    else
+                    {
+                        FOutConvertedTime[i] = conv_DateTimeToTotalSeconds(conv_StringToDateTime_2(FInCurrentTime[i]));
+                        FOutCurrentTime[i] = conv_UTCToTotalSeconds();
+                    }
                 }
                 catch (Exception e)
                 {
-                    // add FLogger here, currently problem using The Import command
-                }
 
+                }
             }
         }
 
         #endregion mainloop
-
-
-        private TimeZoneInfo GetTimeZoneInfoByName(string name)
-        {
-            TimeZoneInfo tz = System.TimeZoneInfo.Local;
-            for (int i = 0; i < tziNames.Length; i++) 
-            {
-                if (name.Equals(tziNames[i]))
-                {
-                    tz = lsTZInfo[i];
-                    break;
-                }
-            }
-            return tz;
-        }
-
-
-        private List<TimeZoneInfo> GetAllTimezones()
-        {
-            List<TimeZoneInfo> ls = new List<TimeZoneInfo>();
-            ReadOnlyCollection<TimeZoneInfo> roc = TimeZoneInfo.GetSystemTimeZones();
-            for (int i = 0; i < roc.Count; i++)
-            {
-                string name = roc[i].DisplayName;
-                ls.Add(roc[i]);
-            }
-            return ls;
-        }
-
 
         private int conv_DateTimeToTotalSeconds(DateTime dt)
         {
@@ -200,56 +129,14 @@ namespace VVVV.Nodes
             return dt;
         }
 
-        private DateTime conv_StringToDateTime_2(string t, string format)
+        private DateTime conv_StringToDateTime_2(string t)
         {
-            int y, M, d, h, m, s, year, month, da, hour, minute, second;
-            DateTime dt;
-            y = format.IndexOf("yyyy");
-            M = format.IndexOf("MM");
-            d = format.IndexOf("dd");
-            h = format.IndexOf("hh");
-            m = format.IndexOf("mm");
-            s = format.IndexOf("ss");
-
-            if (format.Equals(FORMAT_A))
-            {
-                y = 0; M = 5; d = 8; h = 11; m = 14; s = 17;
-                dt = new DateTime(Convert.ToInt32(t.Substring(y, 4)), 
-                                  Convert.ToInt32(t.Substring(M, 2)), 
-                                  Convert.ToInt32(t.Substring(d, 2)), 
-                                  Convert.ToInt32(t.Substring(h, 2)), 
-                                  Convert.ToInt32(t.Substring(m, 2)), 
-                                  Convert.ToInt32(t.Substring(s, 2)));
-            }
-            else if (format.Equals(FORMAT_B)) 
-            {
-                y = 6; M = 3; d = 0; h = 11; m = 14; s = 17;
-                dt = new DateTime(Convert.ToInt32(t.Substring(y, 4)),
-                                  Convert.ToInt32(t.Substring(M, 2)),
-                                  Convert.ToInt32(t.Substring(d, 2)),
-                                  Convert.ToInt32(t.Substring(h, 2)),
-                                  Convert.ToInt32(t.Substring(m, 2)),
-                                  Convert.ToInt32(t.Substring(s, 2)));
-            }
-            else if (format.Equals(FORMAT_C))
-            {
-                y = 0; M = 5; d = 8;
-                dt = new DateTime(Convert.ToInt32(t.Substring(y, 4)),
-                                  Convert.ToInt32(t.Substring(M, 2)),
-                                  Convert.ToInt32(t.Substring(d, 2)));
-            }
-            else if (format.Equals(FORMAT_D))
-            {
-                y = 6; M = 3; d = 0;
-                dt = new DateTime(Convert.ToInt32(t.Substring(y, 4)),
-                                  Convert.ToInt32(t.Substring(M, 2)),
-                                  Convert.ToInt32(t.Substring(d, 2)));
-            }
-            else
-            {
-                dt = new DateTime(1970, 01, 01, 01, 01, 01);
-            }
-            return dt;
+            return new DateTime(Convert.ToInt32(t.Substring(0, 4)), 
+                                Convert.ToInt32(t.Substring(5, 2)), 
+                                Convert.ToInt32(t.Substring(8, 2)), 
+                                Convert.ToInt32(t.Substring(11, 2)), 
+                                Convert.ToInt32(t.Substring(14, 2)), 
+                                Convert.ToInt32(t.Substring(17, 2)));
         }
 
         private int conv_UTCToTotalSeconds()
