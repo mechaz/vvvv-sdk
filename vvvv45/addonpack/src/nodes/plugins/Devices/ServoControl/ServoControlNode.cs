ab1a4d22eb1e5c735d33c5f4f954c62a3bc6a352
Nikos Mechanezidis <mechanezidis@meso.net>
2013-07-12 14:59:42 +0200

    some changes

--- vvvv45/addonpack/src/nodes/plugins/Devices/ServoControl/ServoControlNode.cs
+++ vvvv45/addonpack/src/nodes/plugins/Devices/ServoControl/ServoControlNode.cs
@@ -27,6 +27,10 @@ namespace VVVV.Nodes
         [Input("Set COMPort")]
         IDiffSpread<bool> FInSetCOMPort;
 
+        [Input("Send 0 Pos abs", IsBang=true)]
+        IDiffSpread<bool> FInSendPos0Abs;
+
+
         [Output("Error Int")]
         ISpread<int> FOutErrorInt;
 
@@ -35,6 +39,9 @@ namespace VVVV.Nodes
 
         [Output("IsConnected")]
         ISpread<bool> FOutIsConnected;
+
+        [Output("Error Message")]
+        ISpread<string> FOutErrorMessage;
 
 
 		[Import()]
@@ -61,7 +68,11 @@ namespace VVVV.Nodes
 		public void Evaluate(int SpreadMax)
 		{
             FOutConfig.SliceCount = FOutErrorInt.SliceCount = 1;
+            FOutErrorMessage.SliceCount = 1;
 
+            // -----------------------------------------------------------------------
+            // -----------------------------------------------------------------------
+            
             if (FInSetCOMPort.IsChanged && FInSetCOMPort[0] || FInit)
             {
                 // string conf = "COM5, auto, ESR prot.";
@@ -95,7 +106,94 @@ namespace VVVV.Nodes
             }
             FCOMConfigChanged = false;
             FOutIsConnected[0] = FConnected;
+            
+            // -----------------------------------------------------------------------
+            // -----------------------------------------------------------------------
+
+            if (FInSendPos0Abs.IsChanged && FInSendPos0Abs[0])
+            {
+                DoSetPos("0", true);
+            }
 		}
+
+
+        private void DoSetPos(string text, bool absolute)
+        {
+            int rc;
+            ushort us_status;
+            int pos, vel;
+
+            // Read status word
+            if ((rc = servo.ReadStatusWord(out us_status)) != 0)
+            {
+                FOutErrorMessage[0] = "Reading axis state failed!";
+                return;
+            }
+            // Check for state "operation enabled"
+            if (!Servo.isOperationEnabled(us_status))
+            {
+                // Switch to state "operation enabled"
+                if ((rc = servo.SetStateMachine(Servo.TStateCmd.cmdEnableOperation, 500)) != 0)
+                {
+                    FOutErrorMessage[0] = "Switch to \"Operation enabled\" failed.";
+                    return;
+                }
+            }
+            int r = 2;
+            // Read target position from user entry
+            try
+            {
+                pos = Convert.ToInt32(text);
+                int a = 6;
+            }
+            catch
+            {
+                FOutErrorMessage[0] = "is not a valid number";
+                MessageBox.Show(
+                    "'" + text + "' is not a valid number",
+                    "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Read velocityfrom user entry
+            try
+            {
+                vel = Convert.ToInt32(text);
+            }
+            catch
+            {
+                FOutErrorMessage[0] = "is not a valid number";
+                MessageBox.Show(
+                    "'" + text + "' is not a valid number",
+                    "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Set new velocity
+            if ((rc = servo.SetVelocity(vel)) != 0)
+            {
+                FOutErrorMessage[0] = "Set new velocity failed!";
+                return;
+            }
+
+            // Set new target position
+            if ((rc = servo.SetPos(pos, absolute)) != 0)
+            {
+                FOutErrorMessage[0] = "Set new position failed!";
+            } 
+        }
+
+
+
+
+
+
+
+
+
+
 
 
 
