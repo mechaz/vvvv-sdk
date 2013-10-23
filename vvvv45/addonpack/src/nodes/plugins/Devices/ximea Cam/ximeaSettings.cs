cac48afa379a947509fe2861f99e48e5e171f7a1
Nikos Mechanezidis <mechanezidis@meso.net>
2013-10-23 17:59:30 +0200

    second node for ximea settings

---
+++ vvvv45/addonpack/src/nodes/plugins/Devices/ximea Cam/ximeaSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VColor;
+using VVVV.Utils.VMath;
+using xiApi.NET;
+
+namespace VVVV.Nodes
+{
+    #region PluginInfo
+    [PluginInfo(Name = "ximeaSettings", Category = "Devices", Version = "1.0", Help = "Control settings of a ximea usb 3.0 camera", Tags = "")]
+    #endregion PluginInfo
+
+    public class ximeaSettings : IPluginEvaluate
+    {
+        public void Evaluate(int SpreadMax)
+        {
+
+        }
+    }
+}
