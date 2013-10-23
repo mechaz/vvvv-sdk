5a71e4d993c8a13806c59c56ada67c56b78b0d28
Nikos Mechanezidis <mechanezidis@meso.net>
2013-10-23 17:59:08 +0200

    added xiApi.NET.dll

--- vvvv45/addonpack/src/nodes/plugins/Devices/ximea Cam/ximeaCAM.cs
+++ vvvv45/addonpack/src/nodes/plugins/Devices/ximea Cam/ximeaCAM.cs
@@ -6,6 +6,7 @@ using VVVV.PluginInterfaces.V1;
 using VVVV.PluginInterfaces.V2;
 using VVVV.Utils.VColor;
 using VVVV.Utils.VMath;
+using xiApi.NET;
 
 using VVVV.Core.Logging;
 #endregion usings
@@ -13,16 +14,33 @@ using VVVV.Core.Logging;
 namespace VVVV.Nodes
 {
 	#region PluginInfo
-	[PluginInfo(Name = "ximeaCAM", Category = "Value", Version = "1", Help = "Basic template with one value in/out", Tags = "")]
+	[PluginInfo(Name = "ximeaCAM", Category = "Devices", Version = "1.0", Help = "Control settings of a ximea usb 3.0 camera", Tags = "")]
 	#endregion PluginInfo
 	public class ximeaCAM : IPluginEvaluate
 	{
 		#region fields & pins
-		[Input("Input", DefaultValue = 1.0)]
-		ISpread<double> FInput;
+		
+        [Input("Init", IsSingle=true, IsBang=true, DefaultBoolean=false)]
+        IDiffSpread<bool> FInitCam;
 
-		[Output("Output")]
-		ISpread<double> FOutput;
+        [Input("Width", IsSingle = true, DefaultValue = 2040)]
+        IDiffSpread<float> FInWidth;
+
+        [Input("Height", IsSingle = true, DefaultValue = 1080)]
+        IDiffSpread<float> FInHeight;
+
+        [Input("Exposure", IsSingle = true, DefaultValue = 2000)]
+        IDiffSpread<int> FInExposure;
+
+        [Input("Gain", IsSingle = true, DefaultValue = 5)]
+        IDiffSpread<float> FInGain;
+
+        [Output("Framerate")]
+        ISpread<float> FOutFramerate;
+
+
+        private xiCam myCam = new xiCam();
+        private bool FParamsChanged = false;
 
 		[Import()]
 		ILogger FLogger;
@@ -31,12 +49,59 @@ namespace VVVV.Nodes
 		//called when data for any output pin is requested
 		public void Evaluate(int SpreadMax)
 		{
-			FOutput.SliceCount = SpreadMax;
+			
+            for (int i = 0; i < SpreadMax; i++)
+            {
 
-			for (int i = 0; i < SpreadMax; i++)
-				FOutput[i] = FInput[i] * 2;
+            }
+            if (FInitCam.IsChanged && FInitCam[0])
+            {
+                SetParamsToCam(FInExposure[0], FInGain[0]);
+            }
 
-			//FLogger.Log(LogType.Debug, "hi tty!");
+            if (FInExposure.IsChanged || FInGain.IsChanged || FInHeight.IsChanged || FInWidth.IsChanged)
+            {
+                FParamsChanged = true;
+            }
+            if (FParamsChanged)
+            {
+                SetParamsToCam(FInExposure[0], FInGain[0]);
+                FParamsChanged = false;
+            }
 		}
+
+        private void SetParamsToCam(int exposure, float gain)
+        {
+            try
+            {
+                // Initialize first camera
+                myCam.OpenDevice(0);
+
+                // set device exposure
+                myCam.SetParam(PRM.EXPOSURE, exposure);
+                myCam.SetParam(PRM.HEIGHT, 1080);
+                myCam.SetParam(PRM.WIDTH, 2040);
+                // set gain db
+                myCam.SetParam(PRM.GAIN, gain);
+
+                // Set image output format to RGB32
+                myCam.SetParam(PRM.IMAGE_DATA_FORMAT, IMG_FORMAT.MONO8);
+
+                FLogger.Log(LogType.Debug, "Parameters set ..");
+            }
+
+            catch (System.ApplicationException appExc)
+            {
+                // Show handled error
+                FLogger.Log(LogType.Debug, "Error setting params ..");
+                myCam.CloseDevice();
+            }
+
+            finally
+            {
+                myCam.CloseDevice();
+            }
+        }
+
 	}
 }
