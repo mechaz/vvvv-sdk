4dcc70fba565581d10e4eff4e2f8fb41fff796c7
Nikos Mechanezidis <mechanezidis@meso.net>
2013-10-24 17:37:58 +0200

    adding params ..

---
+++ vvvv45/addonpack/src/nodes/plugins/Devices/ximea Cam/Settings.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VVVV.Nodes
+{
+    class Settings
+    {
+        private string deviceName; // Get
+        public string DeviceName
+        {
+            get { return deviceName; }
+        }
+
+        private string deviceType; // Get
+        public string DeviceType
+        {
+            get { return deviceType; }
+        }
+
+        private string deviceInstancePath; // Get
+        public string DeviceInstancePath
+        {
+            get { return deviceInstancePath; }
+        }
+
+        private int deviceSN; // Get
+        public int DeviceSN
+        {
+            get { return deviceSN; }
+        }
+        
+        private int debugLevel; // Get, Set
+        public int DebugLevel
+        {
+            get { return debugLevel; }
+            set { debugLevel = value; }
+        }
+
+        private int autoBandwidthCalculation; // Get, Set
+        public int AutoBandwidthCalculation
+        {
+            get { return autoBandwidthCalculation; }
+            set { autoBandwidthCalculation = value; }
+        }
+
+        private int exposure; // Get, Set
+        public int Exposure
+        {
+            get { return exposure; }
+            set { exposure = value; }
+        }
+        
+        private int exposureMIN; // Get
+        public int ExposureMIN
+        {
+            get { return exposureMIN; }
+        }
+        
+        private int exposureMAX; // Get
+        public int ExposureMAX
+        {
+            get { return exposureMAX; }
+        }
+
+        private float gain;  // Get, Set
+        public float Gain
+        {
+            get { return gain; }
+            set { gain = value; }
+        }
+        
+        private float gainMIN;  // Get
+        public float GainMIN
+        {
+            get { return gainMIN; }
+        }
+        
+        private float gainMAX;  // Get
+        public float GainMAX
+        {
+            get { return gainMAX; }
+        }
+
+        private int downsampling; // Get, Set
+        public int Downsampling
+        {
+            get { return downsampling; }
+            set { downsampling = value; }
+        }
+
+        private int downsamplingMIN; // Get
+        public int DownsamplingMIN
+        {
+            get { return downsamplingMIN; }
+        }
+
+        private int downsamplingMAX; // Get
+        public int DownsamplingMAX
+        {
+            get { return downsamplingMAX; }
+        }
+
+        private int downsamplingType; // Get, Set, Class DOWNSAMPLING_TYPE
+        public int DownsamplingType
+        {
+            get { return downsamplingType; }
+            set { downsamplingType = value; }
+        }
+
+        private int imageDataFormat; // Get, Set Class IMG_FORMAT
+        public int ImageDataFormat
+        {
+            get { return imageDataFormat; }
+            set { imageDataFormat = value; }
+        }
+
+        private int sensorDataBitDepth; // Get, Set
+        public int SensorDataBitDepth
+        {
+            get { return sensorDataBitDepth; }
+            set { sensorDataBitDepth = value; }
+        }
+
+        private int outputDataBitDepth; // Get, Set
+        public int OutputDataBitDepth
+        {
+            get { return outputDataBitDepth; }
+            set { outputDataBitDepth = value; }
+        }
+
+        private int imageIsColor; // Get
+        public int ImageIsColor
+        {
+            get { return imageIsColor; }
+        }
+
+        private int colorFilterArray; // Get
+
+        public int ColorFilterArray
+        {
+            get { return colorFilterArray; }
+        }
+
+        private float framerate; // Get
+        public float Framerate
+        {
+            get { return framerate; }
+        }
+
+        private float framerateMIN; // Get
+        public float FramerateMIN
+        {
+            get { return framerateMIN; }
+        }
+
+        private float framerateMAX; // Get
+        public float FramerateMAX
+        {
+            get { return framerateMAX; }
+        }
+
+        private int availableBandwidth; // Get
+        public int AvailableBandwidth1
+        {
+            get { return availableBandwidth; }
+        }
+
+        private int availableBandwidthMIN; // Get
+        public int AvailableBandwidthMIN
+        {
+            get { return availableBandwidthMIN; }
+        }
+
+        private int availableBandwidthMAX; // Get
+        public int AvailableBandwidthMAX
+        {
+            get { return availableBandwidthMAX; }
+        }
+
+        private int limitBandwidth; // Get, Set
+        public int LimitBandwidth
+        {
+            get { return limitBandwidth; }
+            set { limitBandwidth = value; }
+        }
+
+        private int limitBandwidthMIN; // Get
+        public int LimitBandwidthMIN
+        {
+            get { return limitBandwidthMIN; }
+        }
+
+        private int limitBandwidthMAX; // Get
+        public int LimitBandwidthMAX
+        {
+            get { return limitBandwidthMAX; }
+        }
+
+        private int offset_X; // Get, Set / must be even
+        public int Offset_X
+        {
+            get { return offset_X; }
+            set { offset_X = value; }
+        }
+
+        private int offset_X_MIN; // Get
+        public int Offset_X_MIN
+        {
+            get { return offset_X_MIN; }
+        }
+
+        private int offset_X_MAX; // Get
+        public int Offset_X_MAX
+        {
+            get { return offset_X_MAX; }
+        }
+
+        private int offset_Y; // Get, Set / must be even
+        public int Offset_Y
+        {
+            get { return offset_Y; }
+            set { offset_Y = value; }
+        }
+
+        private int offset_Y_MIN; // Get
+        public int Offset_Y_MIN
+        {
+            get { return offset_Y_MIN; }
+        }
+
+        private int offset_Y_MAX; // Get
+        public int Offset_Y_MAX
+        {
+            get { return offset_Y_MAX; }
+        }
+
+        private int width; // Get, Set / must be divisible by four
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        private int widthMIN; // Get
+        public int WidthMIN
+        {
+            get { return widthMIN; }
+        }
+
+        private int widthMAX; // Get
+        public int WidthMAX
+        {
+            get { return widthMAX; }
+        }
+
+        private int height; // Get, Set / must be even
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        private int heightMIN; // Get
+        public int HeightMIN
+        {
+            get { return heightMIN; }
+        }
+
+        private int heightMAX; // Get
+        public int HeightMAX
+        {
+            get { return heightMAX; }
+        }
+
+        private float wB_KR; // Get, Set / default 1
+        public float WB_KR
+        {
+            get { return wB_KR; }
+            set { wB_KR = value; }
+        }
+
+        private float wB_KG; // Get, Set / default 1
+        public float WB_KG
+        {
+            get { return wB_KG; }
+            set { wB_KG = value; }
+        }
+
+        private float wB_KB; // Get, Set / default 1
+        public float WB_KB
+        {
+            get { return wB_KB; }
+            set { wB_KB = value; }
+        }
+
+        private int manualWB; // Set / default -
+        public int ManualWB
+        {
+            get { return manualWB; }
+            set { manualWB = value; }
+        }
+
+        private int autoWB; // Get, Set / default 1
+        public int AutoWB
+        {
+            get { return autoWB; }
+            set { autoWB = value; }
+        }
+
+        private float gammaY; // Get, Set / default 1.0  Luminosity Gamma
+        public float GammaY
+        {
+            get { return gammaY; }
+            set { gammaY = value; }
+        }
+
+        private float gammaC; // Get, Set / default 0  Chromaticity Gamma
+        public float GammaC
+        {
+            get { return gammaC; }
+            set { gammaC = value; }
+        }
+
+        private float sharpness; // Get, Set default 0
+        public float Sharpness
+        {
+            get { return sharpness; }
+            set { sharpness = value; }
+        }
+
+        private int aEAG; // Get, Set / automatic exposure gain / default 0 (disabled)
+        public int AEAG
+        {
+            get { return aEAG; }
+            set { aEAG = value; }
+        }
+
+        private float ExposurePriority; // 0,5 - exposure 50%, gain 50% / default 0.8
+        public float ExposurePriority1
+        {
+            get { return ExposurePriority; }
+            set { ExposurePriority = value; }
+        }
+        
+        private int AEMaxLimit; // Get, Set default 1000000 ls
+        
+        private float AGMaxLimit; // Get, Set
+        
+        private float AGMaxLimit; // Get, Set
+        
+        private int AEAGLevel; // Get, Set / default 400
+        
+        private int BPC; // Get, Set / default 0 (disabled)
+        
+        private int IsCooled; // Get / returns 1 if supported
+        
+        private int Cooling; // Get, Set / default 0
+        
+        private float TargetTemp; // Get, Set / default 20
+        
+        private float ChipTemp; // Get, Set default -
+        private float HouseTemp; // Get, Set default -
+        private int HDR; // Get, Set default 0 (disabled) alpha dev state
+        private string APIVersion; // Get
+        private string DriverVersion; // Get
+        private int AvailableBandwidth; // Get
+
+        private float bufferPolicy; // Get(float), Set(int)
+    }
+}
--- vvvv45/addonpack/src/nodes/plugins/Devices/ximea Cam/ximeaSettings.cs
+++ vvvv45/addonpack/src/nodes/plugins/Devices/ximea Cam/ximeaSettings.cs
@@ -17,6 +17,224 @@ namespace VVVV.Nodes
 
     public class ximeaSettings : IPluginEvaluate
     {
+        #region fields & pins
+
+        private string FDeviceName; // Get
+        private string FDeviceType; // Get
+        private string FDeviceInstancePath; // Get
+        private int FDeviceSN; // Get
+        private int FDebugLevel; // Get, Set
+        
+        private int FAutoBandwidthCalculation; // Get, Set
+        
+        private int FExposure; // Get, Set
+        private int FExposureMIN; // Get
+        private int FExposureMAX; // Get
+        
+        private float FGain;  // Get, Set
+        private float FGainMIN;  // Get
+        private float FGainMAX;  // Get
+        
+        private int FDopwnsampling; // Get, Set
+        private int FDopwnsamplingMIN; // Get
+        private int FDopwnsamplingMAX; // Get
+        private int FDownsamplingType; // Get, Set, Class DOWNSAMPLING_TYPE
+        
+        private int FImageDataFormat; // Get, Set Class IMG_FORMAT
+        private int FSensorDataBitDepth; // Get, Set
+        private int FOutputDataBitDepth; // Get, Set
+        private int FImageIsColor; // Get
+        private int FColorFilterArray; // Get
+        private float FFramerate; // Get
+        private float FFramerateMIN; // Get
+        private float FFramerateMAX; // Get
+        private int FAvailableBandwidth; // Get
+        private int FAvailableBandwidthMIN; // Get
+        private int FAvailableBandwidthMAX; // Get
+        private int FLimitBandwidth; // Get, Set
+        private int FLimitBandwidthMIN; // Get
+        private int FLimitBandwidthMAX; // Get
+        private float FBufferPolicy; // Get(float), Set(int)
+        private int FOffset_X; // Get, Set / must be even
+        private int FOffset_X_MIN; // Get
+        private int FOffset_X_MAX; // Get
+        private int FOffset_Y; // Get, Set / must be even
+        private int FOffset_Y_MIN; // Get
+        private int FOffset_Y_MAX; // Get
+        private int FWidth; // Get, Set / must be divisible by four
+        private int FWidthMIN; // Get
+        private int FWidthMAX; // Get
+        private int FHeight; // Get, Set / must be even
+        private int FHeightMIN; // Get
+        private int FHeightMAX; // Get
+
+        private float FWB_KR; // Get, Set / default 1
+        private float FWB_KG; // Get, Set / default 1
+        private float FWB_KB; // Get, Set / default 1
+        private int FManualWB; // Set / default -
+        private int FAutoWB; // Get, Set / default 1
+        private float FGammaY; // Get, Set / default 1.0  Luminosity Gamma
+        private float FGammaC; // Get, Set / default 0  Chromaticity Gamma
+        private float FSharpness; // Get, Set default 0
+        private int FAEAG; // Get, Set / automatic exposure gain / default 0 (disabled)
+        private float FExposurePriority; // 0,5 - exposure 50%, gain 50% / default 0.8
+        private int FAEMaxLimit; // Get, Set default 1000000 ls
+        private float FAGMaxLimit; // Get, Set
+        private float FAGMaxLimit; // Get, Set
+        private int FAEAGLevel; // Get, Set / default 400
+        private int FBPC; // Get, Set / default 0 (disabled)
+        private int FIsCooled; // Get / returns 1 if supported
+        private int FCooling; // Get, Set / default 0
+        private float FTargetTemp; // Get, Set / default 20
+        private float FChipTemp; // Get, Set default -
+        private float FHouseTemp; // Get, Set default -
+        private int FHDR; // Get, Set default 0 (disabled) alpha dev state
+        private string FAPIVersion; // Get
+        private string FDriverVersion; // Get
+        private int FAvailableBandwidth; // Get
+
+        [Output("Device Name")]
+        ISpread<string> FOutDeviceName;
+
+        [Output("Device Type")]
+        ISpread<string> FOutDeviceType;
+
+        [Output("Device Instance Path")]
+        ISpread<string> FOutDeviceInstancePath;
+
+        [Output("Device SN")]
+        ISpread<int> FOutDeviceSN;
+
+        [Output("Debug Level")]
+        ISpread<int> FOutDebugLevel;
+
+        [Output("Auto Bandwidth Calculation")]
+        ISpread<int> FOutABWCalculation;
+
+        [Output("Exposure")]
+        ISpread<int> FOutExposure;
+
+        [Output("Exposure MIN")]
+        ISpread<int> FOutExposureMIN;
+
+        [Output("Exposure MAX")]
+        ISpread<int> FOutExposureMAX;
+
+        [Output("Gain")]
+        ISpread<int> FOutGain;
+
+        [Output("Exposure MIN")]
+        ISpread<int> FOutGainMIN;
+
+        [Output("Exposure MAX")]
+        ISpread<int> FOutGainMAX;
+
+        [Output("DownSampling")]
+        ISpread<string> FOutDownsampling;
+
+        [Output("DownSampling MIN")]
+        ISpread<string> FOutDownsamplingMIN;
+
+        [Output("DownSampling MAX")]
+        ISpread<string> FOutDownsamplingMAX;
+
+        [Output("DownSampling Type")]
+        ISpread<string> FOutDownsamplingType;
+
+        [Output("Image Data Format")]
+        ISpread<string> FOutUImageDataFormat;
+
+        [Output("Image Is Color")]
+        ISpread<bool> FOutIsImageColor;
+
+        [Output("Sensor")]
+        ISpread<bool> FOutIsImageColor;
+
+        [Output("SensorData BitDepth")]
+        ISpread<bool> FOutSensorDataBitDepth;
+
+        [Output("OutputData BitDepth")]
+        ISpread<bool> FOutOutputDataBitDepth;
+
+        [Output("Framerate")]
+        ISpread<float> FOutFramerate;
+
+        [Output("Framerate MIN")]
+        ISpread<float> FOutFramerateMIN;
+
+        [Output("Framerate MAX")]
+        ISpread<float> FOutFramerateMAX;
+
+        [Output("Available Bandwidth")]
+        ISpread<int> FOutAvailableBandwidth;
+
+        [Output("Available Bandwidth MIN")]
+        ISpread<int> FOutAvailableBandwidthMIN;
+
+        [Output("Available Bandwidth MAX")]
+        ISpread<int> FOutAvailableBandwidthMAX;
+
+        [Output("Limit Bandwidth")]
+        ISpread<int> FOutLimitBandwidth;
+
+        [Output("Limit Bandwidth MIN")]
+        ISpread<int> FOutLimitBandwidthMIN;
+
+        [Output("Limit Bandwidth MAX")]
+        ISpread<int> FOutLimitBandwidthMAX;
+
+        [Output("Offset X")]
+        ISpread<int> FOutOffsetX;
+
+        [Output("Offset X MIN")]
+        ISpread<int> FOutOffsetXMIN;
+
+        [Output("Offset X MAX")]
+        ISpread<int> FOutOffsetXMAX;
+
+        [Output("Offset Y")]
+        ISpread<int> FOutOffsetY;
+
+        [Output("Offset Y MIN")]
+        ISpread<int> FOutOffsetYMIN;
+
+        [Output("Offset Y MAX")]
+        ISpread<int> FOutOffsetYMAX;
+
+        [Output("Width")]
+        ISpread<int> FOutWidth;
+
+        [Output("Width MIN")]
+        ISpread<int> FOutWidthMIN;
+
+        [Output("Width MAX")]
+        ISpread<int> FOutWidthMAX;
+
+        [Output("Height")]
+        ISpread<int> FOutHeight;
+
+        [Output("Height MIN")]
+        ISpread<int> FOutHeightMIN;
+
+        [Output("Height MAX")]
+        ISpread<int> FOutHeightMAX;
+
+        [Output("WB_KR")]
+        ISpread<float> FOutWB_KR;
+
+        [Output("WB_KG")]
+        ISpread<float> FOutWB_KG;
+
+        [Output("WB_KB")]
+        ISpread<float> FOutWB_KB;
+
+        [Output("Auto WB")]
+        ISpread<int> FOutAutoWB;
+
+
+        #endregion
+
+
         public void Evaluate(int SpreadMax)
         {
 
